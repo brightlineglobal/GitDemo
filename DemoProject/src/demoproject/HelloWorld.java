7adda380ef230524812767d923b605054f0a9e6d
ibm <gilman7@foxmail.com>
2020-07-11 14:06:42 -0500

    adding new print out line

--- DemoProject/src/demoproject/HelloWorld.java
+++ DemoProject/src/demoproject/HelloWorld.java
@@ -3,6 +3,8 @@ package demoproject;
 public class HelloWorld {
 
 	public static void main (String [] args) {
-	System.out.println("Hello World!");
+	
+		//commit
+		System.out.println("Hello World!");
 }
 }
